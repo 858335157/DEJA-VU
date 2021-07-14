1c894abdd6e264047b7baac135c3730ac1dca29f
王宇杰 <858335157@qq.com>
2021-07-14 10:26:54 +0800

    建立了客户端和服务器项目

---
+++ Code/Server/src/main/java/com/wyj/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.wyj.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
